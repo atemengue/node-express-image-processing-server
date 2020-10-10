2f9433612d1072ce3a16a155d25ba7d4c13d8e82
atemengue <moafembe@gmail.com>
2020-10-10 03:10:49 +0100

    module 2 ok

--- api/app.js
+++ api/app.js
@@ -2,9 +2,14 @@
 
 const express = require('express');
 const path = require('path');
+const { ap } = require('ramda');
 const app = express();
+const router = require('./src/router');
 
 const pathIndex = path.resolve(__dirname, '../client/index.html');
+
+app.use('/', router);
+app.use(express.static(path.resolve(__dirname, 'uploads')));
 app.use('/*', (request, response) => {
   response.sendFile(pathIndex);
 });
